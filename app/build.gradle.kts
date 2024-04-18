8b6ea1453b1606abfcc9b8353213bd982b44e8fb
Anas Daghestani <anas.daghestani2002@gmail.com>
2024-04-18 12:40:38 +0300

    initialising the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.freelancing_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.freelancing_app"
