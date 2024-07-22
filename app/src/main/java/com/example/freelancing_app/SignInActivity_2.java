2d8a57a4fc9fc42a9b0eb9e8be9ebfa9ed62a3fb
Anas Daghestani <anas.daghestani2002@gmail.com>
2024-07-22 23:16:47 +0300

    Java stuff

--- app/src/main/java/com/example/freelancing_app/SignInActivity_2.java
+++ app/src/main/java/com/example/freelancing_app/SignInActivity_2.java
@@ -1,7 +1,9 @@
 package com.example.freelancing_app;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +15,15 @@ public class SignInActivity_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page2_1);
-
+        Intent intent = getIntent();
+        boolean IsCustomer = intent.getBooleanExtra("isCustomer",false);
+        if(IsCustomer){
+            Toast.makeText(SignInActivity_2.this,
+                    "Customer", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(SignInActivity_2.this,
+                    "Service Provider", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
